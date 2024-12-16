144d083b3d0ebc5a673f667fbd2a822c9d426999
msa507 <msa10101962@gmail.com>
2024-12-16 10:30:03 +0500

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -1,17 +1,27 @@
 class Animal:
     alive = True
     fed = False
-    name = []
+
     def __init__(self, name):
         self.name = name
 
     def eat(self, food):
-        if food == Plant.edible:
-            fed = True
-            print(f'{self.name} съел {food.name}')
+        if isinstance(food, Plant):
+            if food.edible:
+                print(f"{self.name} съел {food.name}")
+                self.fed = True
+            else:
+                print(f"{self.name} не стал есть {food.name}")
+                self.alive = False
         else:
-            alive = False
-            print(f'{self.name} не стал есть {food.name}')
+            print(f"{self.name} не может есть {food}")
+
+
+class Plant:
+    edible = False
+
+    def __init__(self, name):
+        self.name = name
 
 
 class Mammal(Animal):
@@ -22,38 +32,29 @@ class Predator(Animal):
     pass
 
 
-class Plant:
-    edible = False
-
-    def __init__(self, name):
-        self.name = name
-
 class Flower(Plant):
-
-    def __init__(self, flower_name):
-        self.name = flower_name
-        print(f' O {flower_name} ')
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = False
 
 
 class Fruit(Plant):
-
-    def __init__(self, fruit_name):
-        self.name = fruit_name
-        print(f' O {fruit_name} ')
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
 
 
 a1 = Predator('Волк с Уолл-Стрит')
-# a2 = Mammal('Хатико')
+a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
-# p2 = Fruit('Заводной апельсин')
+p2 = Fruit('Заводной апельсин')
 
-# print(a1.name)
-# print(p1.name, p2.name)
+print(a1.name)
+print(p1.name)
 
-# print(a1.alive)
-# print(a2.fed)
+print(a1.alive)
+print(a2.fed)
 a1.eat(p1)
-
-# a2.eat(p2)
-# print(a1.alive)
-# print(a2.fed)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
