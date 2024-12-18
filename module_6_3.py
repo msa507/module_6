463ae0f9232ef130762f062328dce93c07debbf6
msa507 <msa10101962@gmail.com>
2024-12-18 13:49:55 +0500

    Add files via upload

--- module_6_3.py
+++ module_6_3.py
@@ -1,62 +1,61 @@
 import random
 
-class Animal:
 
+class Animal:
     live = True
     sound = None
     _DEGREE_OF_DANGER = 0
+    x = 0
+    y = 0
+    z = 0
+    _cards = [x, y, z]
 
     def __init__(self, speed):
-        _coords = [0, 0, 0]
+
         self.speed = speed
 
     def move(self, dx, dy, dz):
-        coords = [dx, dy, dz]
-        if self.speed > 0:
-             _coords = map(lambda x: x * self.speed, coords)
         if dz * self.speed < 0:
-            print("It's too deep, i can't dive :(")
+            print("It's too deep, I can't dive :(")
         else:
             Animal.x = dx * self.speed
             Animal.y = dy * self.speed
             Animal.z = dz * self.speed
-        print(list(_coords))
-        return _coords
 
-    def get_coords(self):
-         pass
+    def get_cords(self):
+        print(f'X: {Animal.x} Y: {Animal.y} Z: {Animal.z}')
 
     def attack(self):
         if self._DEGREE_OF_DANGER < 5:
-            print('Sorry, Im peaceful :)')
+            print("Sorry, I'm peaceful :)")
         else:
-            print('Be careful, Im attacking you 0_0')
+            print("Be careful, I'm attacking you 0_0")
 
     def speak(self):
-        sound = self.sound
-        print(sound)
+        print(self.sound)
 
 
 class Bird(Animal):
     beak = True
-    random_eggs = random.randint(0, 4)
 
-    def lay_eggs(self, random_eggs):
-        print (f'Here are(is) {random_eggs} eggs for you')
+    def lay_eggs(self):
+        random_number = random.randint(1, 4)
+        print(f'Here are(is) {random_number} eggs for you')
+
 
 class AquaticAnimal(Animal):
     _DEGREE_OF_DANGER = 3
 
     def dive_in(self, dz):
-        pass
+        Animal.z = (Animal.z / 2) - abs(dz)
+
 
 class PoisonousAnimal(Animal):
     _DEGREE_OF_DANGER = 8
 
 
-class Duckbill(PoisonousAnimal, Bird, AquaticAnimal ):
+class Duckbill(PoisonousAnimal, Bird, AquaticAnimal):
     sound = "Click-click-click"
-
 
 
 db = Duckbill(10)
@@ -66,5 +65,10 @@ print(db.beak)
 
 db.speak()
 db.attack()
-db.move(1, 2, -3)
 
+db.move(1, 2, 3)
+db.get_cords()
+db.dive_in(6)
+db.get_cords()
+
+db.lay_eggs()
